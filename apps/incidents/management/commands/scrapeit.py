041131a73517bf0f045a5cc15ae43169d1de143a
Nathaniel Lash <lash2@illinois.edu>
2014-02-09 00:19:09 -0600

    The regex was looking for 2013 years. Whoops. Now looks for all YY instances

--- apps/incidents/management/commands/scrapeit.py
+++ apps/incidents/management/commands/scrapeit.py
@@ -76,9 +76,11 @@ class Command(BaseCommand):
 		#maketoday's date 
 
 
+
 		###########################################
 		##Alternatively, I can set up a function through Soup that will grab the urls
-		##DEFINITELY need some way to see whether 
+		##DEFINITELY need some way to see whether it's already been crawled. Could bite us in the ass. 
+		##
 
 
 	def convert_to_text(self):
@@ -104,11 +106,11 @@ class Command(BaseCommand):
 
 			return string
 		
-		print "i'm in something"
+		#print "i'm in something"
 		text_files = glob.glob(directory + '/*.txt')
-		print text_files
+	#	print text_files
 		for t in text_files:
-			print t
+			#print t
 			with open (t, "r") as livefile:
 				data = livefile.read()
 
@@ -117,21 +119,24 @@ class Command(BaseCommand):
 		#	print strip_headers
 			#print strip_headers
 
-			pattern = re.compile('(?=(\d{5}\s+)((.|\n)*?)(LOCATION: )((.|\n)*?)(OCCURRED: )(.*?)(REPORTED: )((.|\n)*?)(OFFICER: )((.|\n)*?)(SUMMARY: )((.|\n)*?)((PROPERTY: )((.|\n)*?))?(PEOPLE: )((.|\n)*?)((ARRESTS: )((.|\n)*?))?(C13-\d{5}|\Z))')
+			pattern = re.compile('(?=(\d{5}\s+)((.|\n)*?)(LOCATION: )((.|\n)*?)(OCCURRED: )(.*?)(REPORTED: )((.|\n)*?)(OFFICER: )((.|\n)*?)(SUMMARY: )((.|\n)*?)((PROPERTY: )((.|\n)*?))?(PEOPLE: )((.|\n)*?)((ARRESTS: )((.|\n)*?))?(C\d{2}-\d{5}|\Z))')
+			#currently, when people is NOT followed by an arrest, shit goes to shit. 
 	#with properties.. (nonfunctional)
 	#(?=(\d{5}\s+)((.|\n)*?)(LOCATION: )((.|\n)*?)(OCCURRED: )(.*?)(REPORTED: )((.|\n)*?)(OFFICER: )((.|\n)*?)(SUMMARY: )((.|\n)*?)((PROPERTY: )((.|\n)*?))?(PEOPLE: )((.|\n)*?)((ARRESTS: )((.|\n)*?))?(C13-\d{5}|\Z))
 	#no property
 	#(?=(\d{5}\s+)((.|\n)*?)(LOCATION: )((.|\n)*?)(OCCURRED: )(.*?)(REPORTED: )((.|\n)*?)(OFFICER: )((.|\n)*?)(SUMMARY: )((.|\n)*?)(PEOPLE: )((.|\n)*?)((ARRESTS: )((.|\n)*?))?(C13-\d{5}|\Z))
-
+			#print strip_headers
 			incidents = pattern.findall(strip_headers)
-#			print incidents
+			#print incidents
 
 			j = 0
 			#write function that eats extra whitespace characters. 
 
 			for i in incidents:
+		#		print i
 				j += 1
-				print j
+			#	print ('This is the whole thing<<<<<<<<<<<<<<<<<<<<<< \n %s \n  >>>>>>>>>>>>>>that is the end' % (i,))
+		#		print j
 				code = clean(i[0].strip())
 #				print code
 				description = clean(i[1].strip())
@@ -147,21 +152,40 @@ class Command(BaseCommand):
 				summary = clean(i[15].strip())
 #				print "Summary"
 				#use re to get rid of \n
-				print summary
+			#	print summary
 
 				properties = i[19].strip()
 		#		print properties
+				#### TEST REGION!!!
+			#	people = i[23].strip()
+			#	arrests = i[27].strip()
+			#	arrests += i[28].strip()
+				#########
+
+				
 				people = i[22].strip()
-		#		print people
+			#	print ('PEOPLE THIS IS THE START OF PEOPLE \n %s \n AND THIS IS THE END OF THE PEOPLE SELECTOR!!!' % people)
 				arrests = i[26].strip()
 				arrests += i[27].strip()
 				#print arrests
 
 
+				#arrest_pattern = re.compile('(.*)(AGE: )(\d+)\s+(SEX: )(M|F)(\s+)(.*)\n(.*)(CHARGE: )(\w+)\s+(.*)\n(.*)(AT: )(.*)(BY: )(.*)')
 				arrest_pattern = re.compile('(.*)(AGE: )(\d+)\s+(SEX: )(M|F)(\s+)(.*)\n(.*)(CHARGE: )(\w+)\s+(.*)\n(.*)(AT: )(.*)(BY: )(.*)')
 				arrests_re = arrest_pattern.findall(arrests)
 				for a in arrests_re:
-					print a
+					arrestee = 	clean(a[0].strip())
+					age = 		clean(a[2].strip())
+					sex = 		clean(a[4].strip())
+					address = 	clean(a[6].strip())
+					charge_text=clean(a[9].strip())
+					charge_code=clean(a[10].strip())
+					arrest_location = clean(a[13].strip())
+					arresting_officer = clean(a[15].strip())
+
+					print arresting_officer
+					print arrest_location
+					#print a
 
 
 
